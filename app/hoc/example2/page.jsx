021b9687bcc7089f7f779579a6d77925e41408bb
monirshimul <monirshimul@gmail.com>
2025-05-01 11:37:13 +0600

    hoc with spread operator added

---
+++ app/hoc/example2/page.jsx
@@ -0,0 +1,29 @@
+"use client";
+
+const Button = ({ children, ...rest }) => (
+  <button
+    className="bg-amber-400 px-10 py-3 m-10 cursor-pointer shadow "
+    onClick={() => console.log("ButtonClick")}
+    {...rest}
+  >
+    {children}
+  </button>
+);
+
+const withClick = (Component) => {
+  const handleClick = () => {
+    console.log("WithClick");
+  };
+  return (props) => {
+    console.log("in the hoc props", props);
+    return <Component onClick={handleClick} {...props} />;
+  };
+};
+
+const MyButton = withClick(Button);
+
+const HocExampleTwo = () => {
+  return <MyButton onClick={() => console.log("AppClick")}>Submit</MyButton>;
+};
+
+export default HocExampleTwo;
